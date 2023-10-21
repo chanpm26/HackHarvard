65baa33ea1e7c5deaf103d9f2fa4fea55589b45d
Paige Chan <little26pc@yahoo.com>
2023-10-21 17:44:14 -0400

    Add enter key functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { displayNumberRange, displayResponse, displayRoundAndTries } from "./dom
 
 let round = 1;
 let submitButton = document.getElementById('submit');
+let inputForm = document.getElementById('player-input');
 let tries, mode;
 
 // function that runs the game
@@ -11,29 +12,47 @@ function playGame(mode, tries) {
     displayRoundAndTries(round, tries);
     displayNumberRange(round);
         let targetNumber = generateRandomNumber(round);
-        let playerGuess;
-        submitButton.addEventListener('click', function() {
-            // allow play if tries remaining
-            if (tries > 0) {
-                playerGuess = acceptPlayerInput();
-                let output = checkPlayerGuess(playerGuess, targetNumber, tries);
-                displayResponse(output);
-                // if correct then move to next round
-                if (output == 0) {
-                    round++;
-                    tries = createTries(mode, round);
-                    playGame(mode, tries);
-                // if incorrect, tries goes down
-                } else {
-                    tries = tries-1;
-                    displayRoundAndTries(round, tries);
-                }
-            } else {
-                // if out of tries, the page refreshes to start over
-                location.reload();
+        console.log(targetNumber);
+        inputForm.addEventListener("keypress", function(event) {
+            if (event.key === "Enter") {
+                // updates the tries # parameter if the guess was incorrect
+                let triesCheck = validateGuess(targetNumber, tries);
+                if (triesCheck == -1) tries--;
             }
-    })
+          });
+        submitButton.addEventListener('click', function() {
+            // updates the tries # parameter if the guess was incorrect
+            let triesCheck = validateGuess(targetNumber, tries);
+            if (triesCheck == -1) tries--;
+    });
     
+}
+
+
+// function to validate the player's guess and controls
+// the display of tries and rounds
+function validateGuess(targetNumber, tries) {
+    let playerGuess, currentTries;
+    currentTries = tries;   
+    if (currentTries > 0) {
+        playerGuess = acceptPlayerInput();
+        let output = checkPlayerGuess(playerGuess, targetNumber, currentTries);
+        displayResponse(output);
+        // if correct then move to next round
+        if (output == 0) {
+            round++;
+            tries = createTries(mode, round);
+            playGame(mode, tries);
+        // if incorrect, tries goes down
+        } else {
+            currentTries--;
+            displayRoundAndTries(round, currentTries);
+            return -1;
+        }
+    } else {
+        // if out of tries, the page refreshes to start over
+        location.reload();
+    }
 }
 
 
